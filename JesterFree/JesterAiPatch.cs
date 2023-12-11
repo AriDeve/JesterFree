8cec9bbf79c7388a36883c46d4772cbfd23131f0
AriDeve <37328719+AriDeve@users.noreply.github.com>
2023-12-11 15:43:22 +0200

    Feat: Add free bird intro as the cranking sound, and add config

--- JesterFree/JesterAiPatch.cs
+++ JesterFree/JesterAiPatch.cs
@@ -1,15 +1,26 @@
+using System.Diagnostics.CodeAnalysis;
 using HarmonyLib;
 using UnityEngine;
 
 namespace JesterFree;
 
 [HarmonyPatch(typeof(JesterAI))]
-public class JesterAiPatch
-{
+public static class JesterAiPatch {
     [HarmonyPatch(nameof(JesterAI.Start))]
     [HarmonyPrefix]
-    public static void FreeJesterPatch(ref AudioClip ___screamingSFX)
-    {
-        ___screamingSFX = JesterFreeBase.NewSfx;
+    public static void FreeJesterPatch(
+        [SuppressMessage("ReSharper", "InconsistentNaming")] ref AudioClip ___popGoesTheWeaselTheme,
+        [SuppressMessage("ReSharper", "InconsistentNaming")] ref AudioClip ___screamingSFX,
+        [SuppressMessage("ReSharper", "InconsistentNaming")] ref AudioSource ___farAudio,
+        [SuppressMessage("ReSharper", "InconsistentNaming")] ref AudioSource ___creatureVoice
+        ) {
+        if (JesterFreeBase.Instance.IntroEnabled.Value) {
+            ___popGoesTheWeaselTheme = JesterFreeBase.Instance.Intro;
+            ___farAudio.volume = JesterFreeBase.Instance.IntroVolume.Value / 100.0f;
+        }
+        if (JesterFreeBase.Instance.SoloEnabled.Value) {
+            ___screamingSFX = JesterFreeBase.Instance.Solo;
+            ___creatureVoice.volume = JesterFreeBase.Instance.SoloVolume.Value / 100.0f;
+        }
     }
 }
--- JesterFree/JesterFreeBase.cs
+++ JesterFree/JesterFreeBase.cs
@@ -1,6 +1,6 @@
-﻿
 using System.IO;
 using BepInEx;
+using BepInEx.Configuration;
 using BepInEx.Logging;
 using HarmonyLib;
 using LCSoundTool;
@@ -10,22 +10,43 @@ namespace JesterFree;
 
 [BepInPlugin(ModGuid, "Jester Free", "1.0.6")]
 [BepInDependency("LCSoundTool")]
-public class JesterFreeBase : BaseUnityPlugin
-{
+public class JesterFreeBase : BaseUnityPlugin {
     private const string ModGuid = "AriDev.JesterFree";
-    
+
+    public static JesterFreeBase Instance { get; private set; }
+
     private readonly Harmony _harmony = new(ModGuid);
 
-    private static JesterFreeBase _instance;
+    public AudioClip Intro { get; private set; }
+    public AudioClip Solo { get; private set; }
     
-    internal static AudioClip NewSfx;
+    public ConfigEntry<bool> IntroEnabled { get; private set; }
+    public ConfigEntry<int> IntroVolume { get; private set; }
+    public ConfigEntry<bool> SoloEnabled { get; private set; }
+    public ConfigEntry<int> SoloVolume { get; private set; }
 
-    private void Awake()
-    {
-        if (_instance == null) _instance = this;
+    private void Awake() {
+        if (Instance != null) {
+            Logger.Log(LogLevel.Info, "Jester Free instance already running");
+            return;
+        }
+        Instance = this;
+        
+        var isEnabled = Config.Bind("Mod", "EnableMod", true, "Enables the mod, otherwise doesn't load it");
+        IntroEnabled = Config.Bind("Sound", "EnableCrankingIntro", true, "Enables the cranking to be replaced by Free Bird intro");
+        IntroVolume = Config.Bind("Sound", "IntroVolume", 50, new ConfigDescription("Sets the volume of the cranking intro (in %)", new AcceptableValueRange<int>(0, 200)));
+        SoloEnabled = Config.Bind("Sound", "EnableScreamSolo", true, "Enables the scream to be replaced by Free Bird solo");
+        SoloVolume = Config.Bind("Sound", "SoloVolume", 100, new ConfigDescription("Sets the volume of the screaming solo (in %)", new AcceptableValueRange<int>(0, 200)));
+        
+        if (!isEnabled.Value) {
+            Logger.Log(LogLevel.Info, "Jester Free disabled in config");
+            return;
+        }
+        
         Logger.Log(LogLevel.Info, "Starting Jester Free");
 
-        NewSfx = SoundTool.GetAudioClip(Path.GetDirectoryName(_instance.Info.Location), "freebird.wav");
+        Intro = SoundTool.GetAudioClip(Path.GetDirectoryName(Info.Location), "intro.wav");
+        Solo = SoundTool.GetAudioClip(Path.GetDirectoryName(Info.Location), "solo.wav");
 
         _harmony.PatchAll(typeof(JesterAiPatch));
         _harmony.PatchAll(typeof(JesterFreeBase));
